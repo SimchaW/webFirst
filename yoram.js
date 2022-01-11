5597ec5114ac47efc9ddb064a997a51f315a0da3
SimchaW <94236610+SimchaW@users.noreply.github.com>
2022-01-11 11:27:44 +0200

    Add files via upload

--- yoram.js
+++ yoram.js
@@ -1,9 +1,7 @@
-const removeHide = (e) => {
-    e.classList.remove("hide");
-}
 
-const addHide = (e) => {
-    e.classList.add("hide");
+const removeOrAddHide = (e) => {
+    e.classList.toggle("hide");
+
 }
 
 
